f159859b967db4024d63cadc021088c06979a4d7
chsunab <chsunab@connect.ust.hk>
2018-01-23 18:04:49 +0800

    finished the seperated

---
+++ Assets/Scripts/SimpleButton.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace CnControls
+{
+
+    public class SimpleButton : MonoBehaviour, IPointerUpHandler, IPointerDownHandler
+    {
+
+        public string ButtonName = "Attack";
+
+        private VirtualButton _virtualButton;
+
+        private void OnEnable()
+        {
+            _virtualButton = _virtualButton ?? new VirtualButton(ButtonName);
+            CnInputManager.RegisterVirtualButton(_virtualButton);
+        }
+
+
+        private void OnDisable()
+        {
+            CnInputManager.UnregisterVirtualButton(_virtualButton);
+        }
+
+
+        public void OnPointerUp(PointerEventData eventData)
+        {
+            _virtualButton.Release();
+        }
+
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            _virtualButton.Press();
+        }
+    }
+}
